a8102ccf5b9018ddbd327afec4540e93c329ab74
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-12 11:44:13 +0900

    Add a template for a rails4 application.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 rails-app-templates
 ===================
 
-my app templates for rails new
+my app templates for rails new.
+
+- rails4-app-template.rb
+  - create git repository
+  - bundle install to ./vendor/bundle
+  - disable assets pipeline
+  - disable turbolinks
+  - add mysql2 gem
+  - use rspec
+
+## Usage
+
+```
+cp /path/to/railsrc ~/.railsrc
+```
+
+adjust the path to rails4-app-template.rb in .railsrc
---
+++ rails4-app-template.rb
@@ -0,0 +1,31 @@
+git :init
+git add: "."
+git commit: %Q{-m 'Initial commit'}
+
+# Ignore /vendor/
+run "cat <<EOF >> .gitignore
+
+/vendor/
+EOF"
+git commit: %Q{-a -m 'Add /vendor/ to .gitignore'}
+
+# Add mysql2 gem
+gem 'mysql2'
+run %Q{echo '' >> Gemfile}
+git commit: %Q{-a -m 'Add mysql2 gem'}
+
+# Disable turbolinks
+run %Q{sed -i '' -e '
+/^# Turbolinks makes following links in your web application faster. Read more: https:\\/\\/github.com\\/rails\\/turbolinks/,/^$/d
+' Gemfile}
+run %Q{sed -i '' -e '
+/^\\/\\/= require turbolinks/d
+' app/assets/javascripts/application.js}
+run %Q{sed -i '' -e '
+s/, \"data-turbolinks-track\" => true//
+' app/views/layouts/application.html.erb}
+git commit: %Q{-a -m 'Disable turbolinks'}
+
+# bundle install
+run "bundle install --path vendor/bundle"
+git commit: %Q{-a -m 'Commit Gemfile.lock'}
